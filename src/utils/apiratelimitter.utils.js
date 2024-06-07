f4feede3c77c531a6a573e5b877bba615f6cba35
adharsh <adharsh@maptell.com>
2024-06-07 14:41:07 +0530

    BUG: API limitter limit figures changed

--- src/utils/apiratelimitter.utils.js
+++ src/utils/apiratelimitter.utils.js
@@ -2,7 +2,8 @@ import { rateLimit } from 'express-rate-limit'
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, 
-	limit: 100,
+	limit: 500,
+	message: 'Too many requests from this IP, please try again after 15 minutes.',
 	standardHeaders: 'draft-7',
 	legacyHeaders: false, 
 })
