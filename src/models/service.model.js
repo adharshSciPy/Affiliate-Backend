8b79a2b406a4c7df173925bcd95da9d4b5ab668b
nandana-2002 <nnandana429@gmail.com>
2024-06-10 15:16:07 +0530

    FEATURE:[17]service schema created

--- src/models/service.model.js
+++ src/models/service.model.js
@@ -1,10 +1,50 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose, { Schema } from 'mongoose';
+
 
 const serviceSchema = new Schema({
     title: {
         type: String,
         required: [true, 'First Name is required']
     },
-})
+    description:{
+        type:String,
+    },
+    duration:{
+        type:String,
+        required: [true, 'duration is required']
+    },
+    price:{
+        type:Number,
+        required: [true, 'price is required']
+
+    },
+    discount:{
+        type:Number,
+        required: [true, 'discount is required']
+    },
+    category:{
+        type:String,
+    },
+    tags:{
+        type:[String],
+        required: [true, 'tags is required']
+    },
+    mode:{
+        type:String,
+    },
+    images:{
+        type:[String],
+    },
+    ratings:{
+        type:String,
+    },
+    CompanyId:{
+        type:String,
+        required: [true, 'companyId is required']
+    }    
+},
+{timestamps:true}
+);
+
 
 export const Service = mongoose.model("Service", serviceSchema)
