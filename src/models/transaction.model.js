6aad00a7711c5ea689b5dd9d978061a29a0ea3ad
nandana-2002 <nnandana429@gmail.com>
2024-06-11 12:21:56 +0530

    FEATURE:[24]transaction schema created

---
+++ src/models/transaction.model.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema } from "mongoose";
+
+const transactionSchema = new Schema({
+  transactionId: {
+    type: String,
+    required: [true, "transactionId is required "],
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, "userId is required"],
+},
+orderId:{
+    type: Schema.Types.ObjectId,
+    ref: 'Order',
+    required: [true, "orderId is required"],
+
+},
+grandTotal:{
+    type:Number,
+    required: [true, "GrandTotal is required"],
+
+
+
+},
+tax:{
+    type:Number,
+    required: [true, "Tax is required"],
+
+},
+paymentMode:{
+    type:String,
+    required: [true, "PaymentMode is required"],
+
+    
+},
+status:{
+    type:String,
+},
+isError:{
+    type:Boolean,
+},
+errorMessage:{
+    type:String,
+
+}
+  
+},
+{timestamps:true}
+
+);
+export const Transaction = mongoose.model("Transaction", transactionSchema);
