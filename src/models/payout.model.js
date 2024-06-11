1b3ef88ed01033353c2216971cf93b3d0e483441
arya-102000 <aryavs819@gmail.com>
2024-06-11 12:22:35 +0530

    FEATURE:[23]payout schema created

---
+++ src/models/payout.model.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+
+const payoutSchema = new Schema(
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: [true, "User id is required"],
+        },
+        companyId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Company',
+            required: [true,"Company id is required"],
+        },
+        amount: {
+            type: Number,
+            trim: true,
+            required: [true, "Amount is required"]
+        },
+        paymentMode: {
+            type: String,
+            required: [ true, "Payment mode is required"]
+        },
+        status: {
+            type: Boolean,
+        },
+        adminId: {
+            type: Schema.Types.ObjectId,
+            ref: 'admin',
+            required: [true,"Admin id is required"],
+        },
+
+
+
+    },
+    { timestamps: true }
+);
+
+export const Payout = mongoose.model("Payout", payoutSchema);
