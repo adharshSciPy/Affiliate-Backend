9678d6a4bbf460e6c3e67a9a6253a24f3cafe174
sanjaysabu1132000 <sanjaysabu7777@gmail.com>
2024-06-11 12:22:32 +0530

    FEATURE: [22] Created Order Schema

---
+++ src/models/order.model.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+const orderSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User Id is required"],
+    },
+    serviceIds: {
+      type: [Schema.Types.ObjectId],
+      ref: "Service",
+      required: [true, "Service Id is required"],
+    },
+    grandTotal: {
+      type: Number,
+      required: [true, "Grand Total is required"],
+    },
+    tax: {
+      type: Number,
+      required: [true, "Tax is required"],
+    },
+    paymentMode: {
+      type: String,
+      required: [true, "Payment Mode is required"],
+    },
+    status: {
+      type: String,
+    },
+    isCancelled: {
+      type: Boolean,
+    },
+  },
+  { timestamps: true }
+);
+export const Order = mongoose.model("Order", orderSchema);
