da47d8820afe14a42263fb4ae6406f3fe7b9cd30
adharsh <adharsh@maptell.com>
2024-06-13 10:41:29 +0530

    FEATURE: paginated get Api for listing all customers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -50,41 +50,79 @@ const registerUser = async (req, res) => {
 //@POST
 // user/login
 // desc:Login api of admin with credentials
-const loginUser = async (req,res) =>{
-  const{email,password} = req.body;
-   
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
   try {
     //sanitiasing inputs
-    const isEmptyFields = [email,password].some((field) => field?.trim() ==="");
-    if(isEmptyFields){
-      return res.status(400).json({ message: 'All fields are required'});
+    const isEmptyFields = [email, password].some((field) => field?.trim() === "");
+    if (isEmptyFields) {
+      return res.status(400).json({ message: 'All fields are required' });
 
     }
     //finding user and validating
-    const user = await User.findOne({email});
-    if(!user) {
+    const user = await User.findOne({ email });
+    if (!user) {
       return res.status(404).json({ message: "User doesn't exist" });
 
     }
     //verify password
 
     const isPasswordCorrect = await user.isPasswordCorrect(password);
-    if(!isPasswordCorrect) {
-      return res.status(401).json ({ message: 'Incorrect password'});
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: 'Incorrect password' });
 
 
     }
     //generate access token
     const accessToken = await user.generateAccessToken();
 
-    return res.status(200).json({message: 'User validation Successful',token: accessToken})
+    return res.status(200).json({ message: 'User validation Successful', token: accessToken })
   } catch (err) {
-    return res.status(500).json({ message:  `Internal Server due to ${err.message}`});
+    return res.status(500).json({ message: `Internal Server due to ${err.message}` });
   }
 }
 
 //@GET
-// user/users
+// user/customers
 // desc: Paginated api for getting all users with role customer
+const getAllCustomers = async (req, res) => {
 
-export { registerUser, loginUser }
+  const { page = 1, limit = 10 } = req.query;
+  const pageNumber = parseInt(page, 10);
+  const limitNumber = parseInt(limit, 10);
+
+  // skip logic
+  const skip = (pageNumber - 1) * limitNumber;
+
+  try {
+
+    // pagination logic
+    const totalCustomers = await User.countDocuments({ role: 'customer' });
+    const totalPages = Math.ceil(totalCustomers / limitNumber);
+    const hasNextPage = pageNumber < totalPages;
+
+    // Find customers with pagination
+    const customers = await User.find({ role: 'customer' })
+      .select('firstName lastName email phone')
+      .skip(skip)
+      .limit(limitNumber);
+
+    // Check if customers were found
+    if (customers.length === 0) {
+      return res.status(404).json({ message: 'No customers found' });
+    }
+
+    // Respond with customer data and pagination info
+    return res.status(200).json({
+      message: 'Customer data found',
+      data: { customers, hasNextPage, totalPages, currentPage: pageNumber }
+    });
+  } catch (err) {
+    // Handle any errors
+    return res.status(500).json({ message: `Internal server error due to: ${err.message}` });
+  }
+}
+
+
+export { registerUser, loginUser, getAllCustomers }
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
-import { registerUser,loginUser } from "../controllers/user.controller.js"
+import { registerUser, loginUser, getAllCustomers } from "../controllers/user.controller.js"
 
 const userRoute = Router()
 
 userRoute.route('/register').post(registerUser)
 userRoute.route('/login').post(loginUser)
+userRoute.route('/customers').get(getAllCustomers)
+// userRoute.route('/affiliaters').get(getAllCustomers)
 
 export default userRoute
