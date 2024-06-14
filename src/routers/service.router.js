63b06ab221402638cf1a84913c789b5024416d70
sanjaysabu1132000 <sanjaysabu7777@gmail.com>
2024-06-14 13:32:07 +0530

    FEATURES: [43] Created paginated GET api for retrieving all services from service schema.

--- src/routers/service.router.js
+++ src/routers/service.router.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { deleteService, postService,serviceMoreDetials } from "../controllers/service.controller.js";
+import { deleteService, postService,UpadateServiceDetials,getAllServices } from "../controllers/service.controller.js";
 
 const serviceRoute = Router();
 
  serviceRoute.route('/services/:companyId').post(postService)
-// serviceRoute.route('/services').get(controllerfunction)
-serviceRoute.route('/services/:serviceId').patch(serviceMoreDetials)
+serviceRoute.route('/services').get(getAllServices)
+serviceRoute.route('/services/:serviceId').patch(UpadateServiceDetials)
 serviceRoute.route('/services/:serviceId').delete(deleteService)
 
 export default serviceRoute
