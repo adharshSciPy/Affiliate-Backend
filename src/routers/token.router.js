1915534163994834f36005f85aecc9bd164659d6
arya <aryavs819@gmail.com>
2024-06-28 16:04:09 +0530

    FEATURE:patch and delete in token

--- src/routers/token.router.js
+++ src/routers/token.router.js
@@ -1,11 +1,13 @@
 import { Router } from "express"
-import { generateToken, updateTokenUseCount ,getAllTokens  } from "../controllers/token.controller.js"
+import { generateToken, updateTokenUseCount ,getAllTokens, updateTokenUsage, deleteToken  } from "../controllers/token.controller.js"
 
 const tokenRoute = Router()
 
 tokenRoute.route('/tokens').post(generateToken)
 tokenRoute.route('/tokens/:tokenId').patch(updateTokenUseCount)
+tokenRoute.route('/tokens/:tokenId').patch(updateTokenUsage)
 tokenRoute.route('/tokens').get(getAllTokens )
+tokenRoute.route('/tokens/:tokenId').delete(deleteToken)
 
 
 export default tokenRoute
