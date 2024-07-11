5d1f89e7cb67fd86b7a51a5529b5d09082dfd2b6
adharsh <adharsh@maptell.com>
2024-07-11 16:37:06 +0530

    MODIFICATION: console cleared

--- src/utils/uniqueToken.js
+++ src/utils/uniqueToken.js
@@ -3,8 +3,6 @@ const generateToken = (firstName, lastName) => {
     const charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     const firstLetter = firstName.charAt(0);
     const lastLetter = lastName.charAt(0).toUpperCase();
-    console.log("name", firstName + lastName)
-    console.log("lastletter", lastLetter)
 
     // Generate a 6-character random alphanumeric token
     let token = '';
