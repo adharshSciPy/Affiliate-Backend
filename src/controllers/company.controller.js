37c276b83f81e46a3f8d53df9028858454eb4ac6
adharsh <adharsh@maptell.com>
2024-07-17 17:07:36 +0530

    FEATURE: personal details

--- src/controllers/company.controller.js
+++ src/controllers/company.controller.js
@@ -369,6 +369,23 @@ const manageCompanyBlock = async (req, res) => {
   }
 };
 
+//@GET
+// company/companies/:companyId
+// desc: get personal information of a company
+const getPersonalInfo = async (req, res) => {
+  const { companyId } = req.params;
+  try {
+    const company = await Company.findById(companyId)
+    if (!company) {
+      return res.status(400).json({ message: "Company not found" });
+    }
+    const getresult = await Company.findById(companyId)
+      .select("firstName lastName DOB Gender phoneNumber emailAddress website Address nationality")
+    return res.status(200).json({ message: "Personal data's found", data: getresult })
+  } catch (error) {
+    return res.status(500).json({ message: `Internal server error due to: ${error.message}` });
+  }
+};
 
 export {
   registerCompany,
@@ -380,5 +397,6 @@ export {
   refreshCompanyAccessToken,
   logoutCompany,
   verifyCompany,
-  manageCompanyBlock
+  manageCompanyBlock,
+  getPersonalInfo
 };
